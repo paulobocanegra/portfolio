64f10c5ff3e35bd30a9a5796410927e6bf5a0ecc
paulobocanegra <paulobocanegra@gmail.com>
2023-02-23 12:06:47 -0800

    remove scrolling funciton

--- assets/js/js-functions.js
+++ assets/js/js-functions.js
@@ -35,28 +35,3 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-
-//About Picture rounding with scrolling
-
-// window.onscroll = function () { scrollFunction() };
-
-// function scrollFunction() {
-//     let flight = document.getElementById("about-image")
-//     if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-//         flight.style.borderRadius = "50%";
-//     } else {
-//         document.getElementById("about-image").style.borderRadius = "0%";
-//     }
-// }
-
-// let typeIn = document.getElementById("type-in")
-// let words = ["Software Developer", "Pilot", "Problem Solver", ]
-// let word = ""
-
-
-// function typer(){
-//     let count = 0;
-//     let demo = "AdventureKidpassword";
-//     typeIn.innerHTML = ""
-
-// }
